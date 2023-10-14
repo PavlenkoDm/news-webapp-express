8ba016bb6012e8a366f3045c2ed6cc221af1a655
PavlenkoDm <dmpavlenko07@gmail.com>
2023-10-14 17:37:05 +0300

    add app file and server start logic

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running... Use API on port: ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
