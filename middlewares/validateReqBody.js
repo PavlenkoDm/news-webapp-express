9c17fdd94a10865d28c02e04bbf9f56644f027ea
PavlenkoDm <dmpavlenko07@gmail.com>
2023-10-27 01:06:51 +0300

    validate middleware change

--- middlewares/validateReqBody.js
+++ middlewares/validateReqBody.js
@@ -2,9 +2,10 @@ const { httpError } = require("../helpers");
 
 const validateReqBody = schema => {
   const foo = (req, res, next) => {
-    if (req.body.length === 0) {
-      next();
-    }
+    // if (Object.keys(req.body).length === 0) {
+    //   next();
+    // }
+    if (req.body.length === 0) next();
     const { error } = schema.validate(req.body);
     if (error) {
       next(httpError(400, error.message));
