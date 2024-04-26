4d1f6d235c46ce7b7a5fb9121e1039228d2f33b7
PavlenkoDm <dmpavlenko07@gmail.com>
2024-04-26 21:27:26 +0300

    add cookies

--- controllers/auth/getCryptoData.js
+++ controllers/auth/getCryptoData.js
@@ -15,7 +15,10 @@ const getCryptoData = async (req, res) => {
   } = user;
 
   if (!encryptedPassword || !salt || !exportedCryptoKey) {
-    throw httpError(404, `Crypto data not found (userId: ${userId}), (user: ${user.email})`);
+    throw httpError(
+      404,
+      `Crypto data not found (userId: ${userId}), (user: ${user.email}), (cookie: ${req.cookies.userId})`
+    );
   }
 
   res.status(200);
